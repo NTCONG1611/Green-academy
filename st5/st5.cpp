b5723e2779c3d6724a0654e79945cb45b6b7c740
cong <you@example.com>
2025-01-08 20:52:24 +0700

    ntcong

--- st5/st5.cpp
+++ st5/st5.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hhghghgtrtrtrtrtr
+    std::cout << "Helllcong !";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -15,5 +15,6 @@ int main()
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
-//   4. Use the Error List wintêtrter to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
